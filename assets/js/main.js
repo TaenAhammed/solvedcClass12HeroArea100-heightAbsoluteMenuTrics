f9bc86597bf6339fdf979141038865c1e28ab8b8
TaenAhammed <32127459+TaenAhammed@users.noreply.github.com>
2018-04-18 18:44:16 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.playBtn').magnificPopup({
+    items: [
+      {
+        src: 'https://www.youtube.com/watch?v=0w4Y6hpK6Eg',
+        type: 'iframe'
+      }
+    ]
+  });
+});
